6c10c7a2c1413a1bae026188f1b787c799449ce6
melanielwoods <woods_melanie@yahoo.com>
2015-10-05 21:15:54 -0400

    Create scipt.js

---
+++ scipt.js
@@ -0,0 +1,6 @@
+menuElement=document.getElementById("logo-area")
+console.log("got the logo element")
+        
+function HandleMenuClick(){                                                                                                                          //document.getElementById('logo-area').innerHTML = 'THIS IS THE NEW MENU TEXT'};
+                    $("#logo-area").html('THIS IS THE NEW MENU TEXT')};
+        //menuElement.innerHTML = "THIS IS THE NEW MENU TEXT";
